9fc96e9068414cb7c0c348bb7ee7c41670347e9c
杨国保 <yangguobao@edaijia-inc.cn>
2020-02-16 19:31:08 +0800

    添加自定义写日志方法，目前只加了access.log写入， 等后面改用koa框架时候会用其他日志插件

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const handleHttp = require('./src/routes/');
 require('./src/utils/logger');
+require('./src/utils/log');
 
 http.createServer(handleHttp).listen(3333);
---
+++ src/utils/log.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+
+// 生成write steam
+const createWriteStram = (filename) => {
+  const fullFileName = path.join(__dirname, '../../logs', filename);
+  const ws = fs.createWriteStream(fullFileName, {flag: 'a'});
+  return ws;
+};
+
+const accessWS = createWriteStram('access.log');
+
+/**
+ * 写访问日志
+ * @param log
+ */
+const access = (log) => {
+  accessWS.write(log + '\n');
+};
+global.access = access;
+
+module.exports = {
+  access
+};
